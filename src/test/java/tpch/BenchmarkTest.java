96cd278785572a847059cd63012cd8e207364984
Grisha Weintraub <grisha.weintraub@gmail.com>
2023-01-09 14:09:43 +0200

    move queries to the new benchmark

--- src/test/java/tpch/BenchmarkTest.java
+++ src/test/java/tpch/BenchmarkTest.java
@@ -16,96 +16,11 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-import static bqcpp.ClauseType.LESS_OR_EQUAL_THAN;
-import static bqcpp.ClauseType.RANGE;
 import static org.apache.spark.sql.functions.*;
 import static testutils.MyUtils.initTestSparkSession;
+import static tpch.BenchmarkNew.*;
 
 public class BenchmarkTest {
-
-    List<Clause> q1 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "969", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-06", "1994-01-10"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-05")
-    );
-
-    List<Clause> q2 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "972", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-10", "1994-01-20"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-09")
-    );
-
-    List<Clause> q3 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "980", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-16", "1994-01-26"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-15")
-    );
-
-    List<Clause> q4 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1001", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-20", "1994-01-30"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-19")
-    );
-
-    List<Clause> q5 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1030", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-24", "1994-02-01"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-23")
-    );
-
-    List<Clause> q6 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1050", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-26", "1994-02-03"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-25")
-    );
-
-    List<Clause> q7 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1079", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-31", "1994-02-06"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-30")
-    );
-
-    List<Clause> q8 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1092", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-31", "1994-02-07"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-30")
-    );
-
-    List<Clause> q9 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1098", null),
-            new Clause(RANGE, "l_shipdate", "1994-01-31", "1994-02-09"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-30")
-    );
-
-    List<Clause> q10 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1105", null),
-            new Clause(RANGE, "l_shipdate", "1994-02-01", "1994-02-12"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-31")
-    );
-
-    List<Clause> q11 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1115", null),
-            new Clause(RANGE, "l_shipdate", "1994-02-01", "1994-02-16"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-31")
-    );
-
-    List<Clause> q12 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1122", null),
-            new Clause(RANGE, "l_shipdate", "1994-02-01", "1994-02-20"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-31")
-    );
-
-    List<Clause> q13 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "1137", null),
-            new Clause(RANGE, "l_shipdate", "1994-02-01", "1994-03-01"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-31")
-    );
-
-    List<Clause> q14 = Arrays.asList(
-            new Clause(LESS_OR_EQUAL_THAN, "l_extendedprice", "2000", null),
-            new Clause(RANGE, "l_shipdate", "1994-02-01", "1994-03-30"),
-            new Clause(RANGE, "l_commitdate", "1994-01-01", "1994-01-31")
-    );
 
     @Ignore
     public void benchmarkTest(){
@@ -116,8 +31,6 @@ public class BenchmarkTest {
         String indexPath = "/Users/grishaw/dev/other/tpch/dbgen/index/v10/lineitem1/";
 
         List<Clause>[] queries = new List[]{q6, q12, q14};
-
-        // TODO check different subsets
 
         List<List<String>> result = new LinkedList<>();
 
