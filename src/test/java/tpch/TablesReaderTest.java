abd57a8391754ad4e450c2cd91f3f99d90ebf022
Grisha Weintraub <grisha.weintraub@gmail.com>
2023-09-22 12:47:06 +0300

    add query 1 validation

--- src/test/java/tpch/TablesReaderTest.java
+++ src/test/java/tpch/TablesReaderTest.java
@@ -12,6 +12,24 @@ import static org.apache.spark.sql.functions.*;
 public class TablesReaderTest {
 
     @Test
+    public void query1Validation(){
+        SparkSession sparkSession = initTestSparkSession("query1Validation");
+
+        Dataset lineItem = TablesReader.readLineItem(sparkSession, "src/test/resources/tables/lineitem.tbl");
+
+        Dataset result = lineItem.filter(col("l_shipdate").leq("1998-09-02"))
+                .groupBy("l_returnflag", "l_linestatus")
+                .agg(sum("l_quantity"), sum("l_extendedprice"),
+                        avg("l_quantity"), avg("l_extendedprice"), avg("l_discount"),
+                        count("l_quantity").as("count_order"))
+                .sort("l_returnflag", "l_linestatus");
+
+        Assertions.assertEquals(1478493L, result.
+                where("l_returnflag = 'A' and l_linestatus = 'F'")
+                .select("count_order").as(Encoders.LONG()).collectAsList().get(0));
+    }
+
+    @Test
     public void query6Validation(){
         SparkSession sparkSession = initTestSparkSession("query6Validation");
 
@@ -25,6 +43,24 @@ public class TablesReaderTest {
     }
 
     @Test
+    public void query1ValidationParquet(){
+        SparkSession sparkSession = initTestSparkSession("query1Validation");
+
+        Dataset lineItem = TablesReader.readLineItemParquet(sparkSession, "src/test/resources/tables/parquet/lineitem/");
+
+        Dataset result = lineItem.filter(col("l_shipdate").leq("1998-09-02"))
+                .groupBy("l_returnflag", "l_linestatus")
+                .agg(sum("l_quantity"), sum("l_extendedprice"),
+                        avg("l_quantity"), avg("l_extendedprice"), avg("l_discount"),
+                        count("l_quantity").as("count_order"))
+                .sort("l_returnflag", "l_linestatus");
+
+        Assertions.assertEquals(1478493L, result.
+                where("l_returnflag = 'A' and l_linestatus = 'F'")
+                .select("count_order").as(Encoders.LONG()).collectAsList().get(0));
+    }
+
+    @Test
     public void query6ValidationParquet(){
         SparkSession sparkSession = initTestSparkSession("query6ValidationParquet");
 
@@ -35,6 +71,24 @@ public class TablesReaderTest {
                 .agg(sum(col("l_extendedprice").multiply(col("l_discount")))).as(Encoders.DOUBLE()).collectAsList().get(0);
 
         Assertions.assertEquals(123141078.23, query6ExpectedResult, 0.1);
+    }
+
+    @Test
+    public void query1ValidationIceberg(){
+        SparkSession sparkSession = initTestSparkSession("query1Validation");
+
+        Dataset lineItem = TablesReader.readLineItemIceberg(sparkSession);
+
+        Dataset result = lineItem.filter(col("l_shipdate").leq("1998-09-02"))
+                .groupBy("l_returnflag", "l_linestatus")
+                .agg(sum("l_quantity"), sum("l_extendedprice"),
+                        avg("l_quantity"), avg("l_extendedprice"), avg("l_discount"),
+                        count("l_quantity").as("count_order"))
+                .sort("l_returnflag", "l_linestatus");
+
+        Assertions.assertEquals(1478493L, result.
+                where("l_returnflag = 'A' and l_linestatus = 'F'")
+                .select("count_order").as(Encoders.LONG()).collectAsList().get(0));
     }
 
     @Test
